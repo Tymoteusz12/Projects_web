2ba6382e8e8b6e9aea468d4dc7954975fc1a77aa
Tymek Jagiello <tymekj23@interia.pl>
2020-09-01 00:00:51 +0200

    Updates

---
+++ precache-manifest.8950f1517205a7fc5f0d75213bc99f9c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a73c9894cf02f80a4398bcb2691bb8b3",
+    "url": "/Projects_web/index.html"
+  },
+  {
+    "revision": "479c11b5df6671868b27",
+    "url": "/Projects_web/static/css/2.40a775a0.chunk.css"
+  },
+  {
+    "revision": "bce8eb56b501ba36fe28",
+    "url": "/Projects_web/static/css/main.81937ea1.chunk.css"
+  },
+  {
+    "revision": "479c11b5df6671868b27",
+    "url": "/Projects_web/static/js/2.0a6f0690.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Projects_web/static/js/2.0a6f0690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce8eb56b501ba36fe28",
+    "url": "/Projects_web/static/js/main.cd48c2fe.chunk.js"
+  },
+  {
+    "revision": "4f19640879f1c1ac8d32",
+    "url": "/Projects_web/static/js/runtime-main.95c05238.js"
+  },
+  {
+    "revision": "e6d437014c577b504c92a61751d5cca6",
+    "url": "/Projects_web/static/media/gallery.e6d43701.png"
+  },
+  {
+    "revision": "31839cfad484e0025e57ad556f9a9156",
+    "url": "/Projects_web/static/media/idea.31839cfa.png"
+  },
+  {
+    "revision": "5a99712e982491ce17d9f14dee88dddd",
+    "url": "/Projects_web/static/media/info.5a99712e.png"
+  },
+  {
+    "revision": "9796a42439ce3ea092eb94d95cfee4d7",
+    "url": "/Projects_web/static/media/info_bgc.9796a424.png"
+  },
+  {
+    "revision": "89be230d316e26149d59fd56ec19d065",
+    "url": "/Projects_web/static/media/passion.89be230d.png"
+  }
+]);
